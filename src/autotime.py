929d88377ad36495835d424b11193b42a601b269
Sergey Alexandrov <alexandrov88@gmail.com>
2012-12-13 22:57:39 +0100

    Autotime node refactoring.

--- src/autotime.py
+++ src/autotime.py
@@ -12,31 +12,43 @@ import dynamic_reconfigure.client
 from pid import PID
 
 
-class Fitness:
+class IntegrationTime:
+
+    MIN = 30
+    MAX = 2000
+
     def __init__(self):
         self.client = dynamic_reconfigure.client.Client("camera/driver",
-                                                        timeout=30)
+                                                        timeout=1)
+
+    def set(self, time):
+        time = min(self.MAX, max(self.MIN, time))
+        self.client.update_configuration({"integration_time": time})
+
+    def get(self):
+        return self.client.get_configuration()['integration_time']
+
+    def add(self, time):
+        current = self.get()
+        self.set(current + time)
+
+
+class SaturationPID:
+    def __init__(self):
+        self.time = IntegrationTime()
         self.saturated_sub = rospy.Subscriber('camera/saturation_filter/'
                                               'saturated_pixels', UInt32,
                                               self.saturated_cb)
         self.pid = PID(0.1, 0.0, 0.1)
         self.pid.setPoint(00)
 
-    def set_integration_time(self, parameter):
-        self.client.update_configuration({"integration_time": parameter})
-
     def saturated_cb(self, msg):
         pid = self.pid.update(msg.data)
-        rospy.loginfo('Saturated: %f, Control: %f' % (msg.data, pid))
-        time = self.client.get_configuration()['integration_time'] + pid
-        if time < 30:
-            time = 30
-        if time > 2000:
-            time = 2000
-        self.set_integration_time(time)
+        rospy.loginfo('Saturated pixels: %f, Control: %f' % (msg.data, pid))
+        self.time.add(pid)
 
 if __name__ == '__main__':
     rospy.init_node(NODE)
-    fitness = Fitness()
+    saturation_pid = SaturationPID()
     rospy.loginfo('Started autotime node')
     rospy.spin()
